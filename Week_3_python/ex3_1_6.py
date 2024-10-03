f8d9f906eb5edd9e0115c88fb3586b9b154bdb78
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-10-03 08:07:03 +0200

    week 3 done

--- Week_3_python/ex3_1_6.py
+++ Week_3_python/ex3_1_6.py
@@ -19,9 +19,18 @@ i_end = np.where(hospitalized_data[:, 0] == '2021-02-28')[0][0]
 hospitalized_data = hospitalized_data[i_start:i_end, -1].astype(int)
 #%%
 k = int(positives_data.shape[0]/2)
-
-r = xcorr(positives_data, hospitalized_data, k)
 uk = np.arange(-k,k+1)
-plt.plot(uk, r, color = 'b', label='N=30 a=0.1')
+r = xcorr(positives_data, hospitalized_data, k)
+
+fig, ax = plt.subplots(3, 1, figsize=(14, 20))
+ax[0].plot(positives_data)
+ax[0].set_title('Positive cases')
+ax[0].set_xlabel('Time (Days)')
+ax[1].plot(hospitalized_data)
+ax[1].set_title('Hospitalized cases')
+ax[1].set_xlabel('Time (Days)')
+ax[2].plot(uk, r)
+ax[2].set_title('Cross-correlation')
+ax[2].set_xlabel('Lag of positives to hospitalized cases (Days)')
 
 # %%
